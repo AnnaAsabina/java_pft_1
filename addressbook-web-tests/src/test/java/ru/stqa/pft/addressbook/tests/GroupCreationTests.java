d04e82ddb27d073f5a7baf8deeff589624be165b
AnnaAsabina <a.asabina@quality-lab.ru>
2018-06-02 23:27:56 +0300

    Hamcrest: улучшение внешнего вида проверок для тестов Групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,31 +1,27 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-
-import java.util.Comparator;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.goTo().gotoGroupPage();
-    Set<GroupData> before = app.group().all();
+     Groups before = app.group().all();
     GroupData group = new GroupData().withName("test2");
     app.group().create(group);
     Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(),before.size()+1);
+    assertThat(after.size(),equalTo(before.size()+1));
 
-    group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
-
-    group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt());
-    before.add(group);
-
-    Comparator<? super GroupData> byId =(g1, g2) -> Integer.compare(g1.getId(),g2.getId());
-    Assert.assertEquals(before,after);
+    assertThat(after,
+            equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
   }
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -5,10 +5,15 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupModificationTests extends TestBase {
 
@@ -23,19 +28,16 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
 
-    Set<GroupData> before = app.group().all();
+    Groups before = app.group().all();
     GroupData modifiedGroup =  before.iterator().next();
 
     GroupData group = new GroupData()
             .withId(modifiedGroup.getId()).withName("NameTest").withHeader( "HeaderTest").withFooter("FooterTest");
     app.group().modify(group);
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(),before.size());
+    Groups after = app.group().all();
+    assertEquals(after.size(),before.size());
 
-    before. remove (modifiedGroup);
-    before.add(group);
-    Comparator<? super GroupData> byId =(g1, g2) -> Integer.compare(g1.getId(),g2.getId());
-    Assert.assertEquals(before, after);
+    assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 
 
