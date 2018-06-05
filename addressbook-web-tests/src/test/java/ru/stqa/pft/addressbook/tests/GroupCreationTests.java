a583e407978e9a7b8ac748ecaf83926e74da5524
AnnaAsabina <a.asabina@quality-lab.ru>
2018-06-05 20:55:12 +0300

    Лекция 5.8. Хеширование и предварительные проверки для тестов групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -11,17 +11,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test (enabled = true)
+  @Test(enabled = true)
   public void testGroupCreation() {
     app.goTo().gotoGroupPage();
-     Groups before = app.group().all();
+    Groups before = app.group().all();
     GroupData group = new GroupData().withName("test2");
     app.group().create(group);
+    assertThat(app.group().count(),equalTo( before.size() + 1));
     Set<GroupData> after = app.group().all();
-    assertThat(after.size(),equalTo(before.size()+1));
 
     assertThat(after,
-            equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
-  }
+            equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
+
+  @Test(enabled = true)
+  public void testBadGroupCreation() {
+    app.goTo().gotoGroupPage();
+    Groups before = app.group().all();
+    GroupData group = new GroupData().withName("test2'");
+    app.group().create(group);
+    assertThat(app.group().count(),equalTo( before.size() + 1));
+    Set<GroupData> after = app.group().all();
+    assertThat(after.size(), equalTo(before.size()));
+
+    assertThat(after,
+            equalTo(before));
+  }
+
+}
