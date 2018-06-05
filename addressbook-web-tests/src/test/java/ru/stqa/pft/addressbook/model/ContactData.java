82d228956f72976b387170d76b4ad6f6f2e8ee6b
AnnaAsabina <a.asabina@quality-lab.ru>
2018-06-06 00:49:59 +0300

    ArrayIndexOutOfBoundExeption и с чем его едят...?

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.model;
 public class ContactData {
 
 
-
   private  String firstName;;
   private  String middleName;
   private  String lastName;
@@ -15,62 +14,30 @@ public class ContactData {
   private int index;
 
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
 
-    ContactData that = (ContactData) o;
+  private String allPhones;
+  private String homePhone;
+  private String mobilePhone;
+  private String workPhone;
+  private String allEmails;
+  private String email1;
+  private String email2;
+  private String email3;
 
-    if (id != that.id) return false;
-    if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
-    return lastName != null ? lastName.equals(that.lastName) : that.lastName == null;
+
+  public ContactData withHomePhone(String homePhone) {
+    this.homePhone = homePhone;
+    return this;
   }
 
-  @Override
-  public int hashCode() {
-    int result = firstName != null ? firstName.hashCode() : 0;
-    result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-    result = 31 * result + id;
-    return result;
+  public ContactData withMobilePhone(String mobilePhone) {
+    this.mobilePhone = mobilePhone;
+    return this;
   }
 
-  public String getFirstName() {
-    return firstName;
-  }
 
-  public String getMiddleName() {
-    return middleName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public String getMobilePhone() {
-    return mobile;
-  }
-
-  public String geteMail() {
-    return eMail;
-  }
-
-  public String getGroup() {
-    return group;
-  }
-
-  public int getId() { return id; }
-
-  public int getIndex() {
-    return index;
-  }
-
-  public ContactData withIndex(int index) {
-    this.index = index;
+  public ContactData withWorkPhone(String workPhone) {
+    this.workPhone = workPhone;
     return this;
   }
 
@@ -111,6 +78,124 @@ public class ContactData {
   public ContactData withGroup(String group) {
     this.group = group;
     return this;
+  }
+
+  public ContactData withAllEmails(String allEmails) {
+    this.allEmails = allEmails;
+    return this;
+  }
+
+
+  public ContactData withEmail1(String email1) {
+    this.email1 = email1;
+    return this;
+  }
+
+
+  public ContactData withEmail2(String email2) {
+    this.email2 = email2;
+    return this;
+  }
+
+
+  public ContactData withEmail3(String email3) {
+    this.email3 = email3;
+    return this;
+  }
+
+
+  public ContactData withAllPhones(String allPhones) {
+    this.allPhones = allPhones;
+    return this;
+  }
+
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobilePhone() {
+    return mobile;
+  }
+
+  public String geteMail() {
+    return eMail;
+  }
+
+  public String getGroup() {
+    return group;
+  }
+
+  public int getId() { return id; }
+
+  public int getIndex() {
+    return index;
+  }
+
+  public ContactData withIndex(int index) {
+    this.index = index;
+    return this;
+  }
+
+  public String getHomePhone() {
+    return homePhone;
+  }
+
+  public String getWorkPhone() {
+    return workPhone;
+  }
+
+  public String getAllEmails() {
+    return allEmails;
+  }
+
+  public String getEmail1() {
+    return email1;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+
+  public String getEmail3() {
+    return email3;
+  }
+
+  public String getAllPhones() {
+    return allPhones;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (id != that.id) return false;
+    if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+    return lastName != null ? lastName.equals(that.lastName) : that.lastName == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstName != null ? firstName.hashCode() : 0;
+    result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+    result = 31 * result + id;
+    return result;
   }
 
   @Override
