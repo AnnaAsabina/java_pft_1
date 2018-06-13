83133fb0aa45c60f83dc0a05a58988bc4a9d6958
AnnaAsabina <a.asabina@quality-lab.ru>
2018-06-13 22:17:49 +0300

    Задание №13: Параметризовать тесты для создания групп и контактов
    - странное исключение вTestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,42 +7,51 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   WebDriver wd;
-
+  private final Properties properties;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
   private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser) throws IOException {
     this.browser = browser;
+    properties =new Properties();
   }
 
 
-  public void init() {
+  public void init() throws IOException {
 
-    if(browser.equals( BrowserType.FIREFOX)){
+    String target = System.getProperty("target", "local");
+
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
+
+    if (Objects.equals(browser, BrowserType.FIREFOX)){
       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    }
-    else if (browser.equals(BrowserType.CHROME)){
+    } else if (Objects.equals(browser, BrowserType.CHROME)){
       wd = new ChromeDriver();
-
-    }else if (browser.equals(BrowserType.IE)){
+    } else if (Objects.equals(browser, BrowserType.IE)){
       wd = new InternetExplorerDriver();
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
   }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,9 +5,13 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
-public class TestBase {
+import static java.lang.System.getProperty;
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+
+public class TestBase{
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
