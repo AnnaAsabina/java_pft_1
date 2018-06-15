dbb5402f7916b72f9612894c76fffc19e1005b26
AnnaAsabina <a.asabina@quality-lab.ru>
2018-06-15 18:59:35 +0300

    Лекция 6.11. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -72,7 +74,9 @@ public class GroupCreationTests extends TestBase {
   }
 
   @Test(dataProvider = "validGroupsFromJson")
+
   public void testGroupCreation(GroupData group) {
+
     app.goTo().gotoGroupPage();
     Groups before = app.group().all();
     app.group().create(group);
@@ -81,6 +85,7 @@ public class GroupCreationTests extends TestBase {
 
     assertThat(after,
             equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
   }
 
 
