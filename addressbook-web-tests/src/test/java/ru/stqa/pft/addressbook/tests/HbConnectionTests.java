04f77b6d4418980c3dd1f6aa0c206bf0f638f617
AnnaAsabina <a.asabina@quality-lab.ru>
2018-06-19 16:20:39 +0300

    7.2 лекция. Не импортируются классы hibernate

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTests.java
@@ -0,0 +1,33 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.hibernate.MetadataSources;
+
+
+public class HbConnectionTests {
+  private Object sessionFactory;
+
+  @BeforeClass
+
+  protected void setUp() throws Exception {
+    // A SessionFactory is set up once for an application!
+    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure() // configures settings from hibernate.cfg.xml
+            .build();
+    try {
+      sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+    }
+    catch (Exception e) {
+      // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+      // so destroy it manually.
+      StandardServiceRegistryBuilder.destroy( registry );
+    }
+  }
+
+
+  @Test
+  public void hbConnection(){
+
+  }
+}
