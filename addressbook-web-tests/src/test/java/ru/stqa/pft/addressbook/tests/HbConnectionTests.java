aeca63ed5844ebf6b0581f76a328b89247622f3b
AnnaAsabina <a.asabina@quality-lab.ru>
2018-06-19 18:17:26 +0300

    Лекция 7.2. Исключение и странность с методом commit

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTests.java
@@ -1,12 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
 
 
 public class HbConnectionTests {
-  private Object sessionFactory;
+
+
+  private SessionFactory sessionFactory;
 
   @BeforeClass
 
@@ -21,6 +31,7 @@ public class HbConnectionTests {
       sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
     }
     catch (Exception e) {
+      e.printStackTrace();
       // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
       // so destroy it manually.
       StandardServiceRegistryBuilder.destroy( registry );
@@ -29,9 +40,19 @@ public class HbConnectionTests {
 
 
 
-
   @Test
-  public void hbConnection(){
+  public void hbConnection() {
+
+
+
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<GroupData> result = session.createQuery("from GroupData").list();
+    for (GroupData group :  result) {
+      System.out.println(group);
+    }
+    session.getTransaction().commit();
+    session.close();
 
   }
-}
+  }
