933296dbd3b6c05e525930335bce5a845d9a970c
AnnaAsabina <a.asabina@quality-lab.ru>
2018-06-24 20:40:03 +0300

    8.5 Лекция 8.5. Передача файлов на удалённую машину по протоколу FTP
    почему-то не отключилась капча

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class TestBase {
 
@@ -14,10 +17,13 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("src/test/resources/confiq_inc.php"),"confiq_inc.php","confiq_inc.php.bak");
   }
 
   @AfterSuite (alwaysRun = true)
-  public void tearDown() {
+
+  public void tearDown() throws IOException {
+    app.ftp().restore("confiq_inc.php.bak","confiq_inc.php");
     app.stop();
   }
 
